539139690aa55061d3557b8ac682752c0473ab8e
MoritzGruber <moritzgruber@yahoo.de>
2016-07-19 18:40:31 +0200

    remove alert

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
  angular.module('myApp', []).controller('myCtrl', function($scope) {
       var socket = io.connect(location.host);  //need to be dynamic if the the pi ip changes
-     alert(location.host+":3000");
       $scope.doClick = function(trigger){
           socket.emit(trigger);
         };
