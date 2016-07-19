80d53f96eaa4b2ed32283c6f897bcc6ca13d75a8
MoritzGruber <moritzgruber@yahoo.de>
2016-07-19 18:39:17 +0200

    io cnnect to dynamic host ip

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
  angular.module('myApp', []).controller('myCtrl', function($scope) {
-      var socket = io.connect('http://localhost:3000');
+      var socket = io.connect(location.host);  //need to be dynamic if the the pi ip changes
+     alert(location.host+":3000");
       $scope.doClick = function(trigger){
           socket.emit(trigger);
         };
