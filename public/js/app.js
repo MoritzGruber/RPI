b8fe68df78942055e44eba1311937a96045d0106
MoritzGruber <moritzgruber@yahoo.de>
2016-10-11 22:14:48 +0200

    changed functionality to work with all pins and you pass in the pin as the mode via socket

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,13 @@
  angular.module('myApp', []).controller('myCtrl', function($scope) {
       var socket = io.connect(location.host);  //need to be dynamic if the the pi ip changes
-      $scope.doClick = function(trigger){
-          socket.emit(trigger);
+     $scope.isHigh = false;
+     $scope.isHigh = true;
+     $scope.isInputMode = false;
+      $scope.set = function(pinNumber, isHigh, isInputMode){
+          if(pinNumber == null){pinNumber = 0}
+          socket.emit('set', pinNumber, isHigh, isInputMode);
         };
+        socket.on('change', function (val, pinNumber) {
+            console.log('val = '+val +' pinNumber'+ pinNumber);
+        })
     });
--- server.js
+++ server.js
@@ -13,65 +13,42 @@ app.use('/node_modules', express.static(__dirname + '/node_modules'));
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
-
+//config
+var numberOfPins = 40;
 //gpio configuration
 //using bcm gpio pins
-// PINS USED: IN1:2 IN2:3 IN3:4 IN4:14  , these named are based on the names of the L298n motor controller
-var IN1 = gpio.export(2, {});
-var IN2 = gpio.export(3, {});
-var IN3 = gpio.export(4, {});
-var IN4 = gpio.export(14, {});
-
+var pins = [];
+for (var i = 0; i < numberOfPins; i++) {
+    pins[i] = gpio.export(i+1, {});
+}
 exitHook(function () {
-    console.log('exiting');
-    var IN1 = gpio.unexport(2, {});
-    var IN2 = gpio.unexport(3, {});
-    var IN3 = gpio.unexport(4, {});
-    var IN4 = gpio.unexport(14, {});
+    console.log('exiting..');
+    for (var i = 0; i < numberOfPins; i++) {
+        pins[i] = gpio.unexport(i+1, {});
+    }
+    console.log('Left and reset all GPIO-Pins');
 });
-
-//get called if somebody connects via socket.io to our ip
-//used for fast, live control via web clint
+//get called if clint connects via socket.io to our ip
 io.on('connection', function (socket) {
-    socket.on('foreward', function () {
-        console.log("moving forward");
-        IN1.set(1);
-        IN2.set(0);
-        IN3.set(1);
-        IN4.set(0);
+    socket.on('set', function (pinNumber, isHigh, isInputMode) {
+        if(isInputMode){
+            pins[pinNumber].setDirection("out");
+            if (isHigh) {
+                pins[pinNumber].set();
+            } else {
+                pins[pinNumber].set(0);
+            }
+        } else {
+            pins[pinNumber].setDirection("in");
+            pins[pinNumber].on("change", sendChangedPinWithValue);
+        }
     });
-    socket.on('left', function () {
-        console.log("turning left");
-        IN1.set(1);
-        IN2.set(0);
-        IN3.set(0);
-        IN4.set(0);
-    });
-    socket.on('stop', function () {
-        console.log("stopping... ");
-        IN1.set(0);
-        IN2.set(0);
-        IN3.set(0);
-        IN4.set(0);
-    });
-    socket.on('right', function () {
-        console.log("turning right");
-        IN1.set(0);
-        IN2.set(0);
-        IN3.set(1);
-        IN4.set(0);
-    });
-
-    socket.on('backward', function () {
-        console.log("moving backward");
-        IN1.set(0);
-        IN2.set(1);
-        IN3.set(0);
-        IN4.set(1);
-    });
-    console.log("a client joined");
+    var sendChangedPinWithValue = function (val) {
+        socket.emit('change', val);
+    };
+    console.log('a client joined');
     socket.on('disconnect', function () {
-        console.log("a client left")
+        console.log('a client left')
     });
 });
 
